7051953e1004722394711e0abce1e44327872d4d
adjellogarfo <adjellogarfo@gmail.com>
2024-01-06 12:22:41 -0800

    Initial commit

---
+++ src/views/experts/AddExpert.vue
@@ -0,0 +1,126 @@
+<template>
+  <section class="page-section">
+    <b-container>
+      <HeaderPage title="Adicionar Expert" />
+      <!--FORM-->
+      <b-row>
+        <b-col cols="2"></b-col>
+        <b-col cols="8">
+          <form @submit.prevent="add">
+            <div class="form-group">
+              <input
+                v-model="name"
+                type="text"
+                class="form-control form-control-lg"
+                id="txtName"
+                placeholder="escreve nome"
+                required
+              />
+            </div>
+            <div class="form-group">
+              <select
+                id="sltType"
+                class="form-control form-control-lg"
+                v-model="type"
+                required
+              >
+                <option value="">-- SELECIONA ESPECIALIDADE --</option>
+                <option value="anfibio">ANFÍBIO</option>
+                <option value="ave">AVE</option>
+                <option value="mamífero">MAMÍFERO</option>
+                <option value="peixe">PEIXE</option>
+                <option value="reptil">RÉPTIL</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <input
+                v-model="email"
+                type="email"
+                class="form-control form-control-lg"
+                id="txtEmail"
+                placeholder="escreve e-mail"
+                
+              />
+            </div>
+            <div class="form-group">
+              <input
+                v-model="address"
+                type="text"
+                class="form-control form-control-lg"
+                id="txtAddress"
+                placeholder="escreve morada"
+                
+              />
+            </div>  
+
+            <button type="submit" class="btn btn-outline-success btn-lg mr-2">
+              <i class="fas fa-plus-square"></i> ADICIONAR
+            </button>
+            <router-link
+              :to="{ name: 'listExperts' }"
+              tag="button"
+              class="btn btn-outline-danger btn-lg"
+            >
+              <i class="fas fa-window-close"></i> CANCELAR
+            </router-link>
+          </form>
+        </b-col>
+        <b-col cols="2"></b-col>
+      </b-row>
+    </b-container>
+  </section>
+</template>
+
+<script>
+/*++++++++++++++++ TODO quando back-end +++++++++++++++++++++++++++++++++ */
+import { ADD_USER } from "@/store/users/user.constants";
+import HeaderPage from "@/components/HeaderPage.vue";
+import router from "@/router";
+import { mapGetters } from "vuex";
+
+export default {
+  name: "AddUser",
+  components: {
+    HeaderPage,
+  },
+  data: () => {
+    return {
+      location: { city: "", district: "", country: "" },
+      auth: { username: "", password: "" },
+      gamification: { points: "", quiz: "" },
+      active: true,
+      name: "",
+      type: "",
+      birth_date: "",
+      description: "",
+    };
+  },
+  computed: {
+    ...mapGetters("user", ["getMessage"]),
+  },
+  methods: {
+    add() {
+      if (
+        document.querySelector("#txtPassword").value !==
+        document.querySelector("#txtConfirmPassword").value
+      ) {
+        this.$alert(
+          "Campos password não coincidem",
+          "Erro de validação do formulário",
+          "error"
+        );
+      } else {
+        this.$store.dispatch(`user/${ADD_USER}`, this.$data).then(
+          () => {
+            this.$alert(this.getMessage, "Utilizador adicionado!", "success");
+            router.push({ name: "listUsers" });
+          },
+          (err) => {
+            this.$alert(`${err.message}`, "Erro", "error");
+          }
+        );
+      }
+    },
+  },
+};
+</script>
